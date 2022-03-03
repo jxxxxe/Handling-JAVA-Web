5c40a7ac0c752b3f8ba10b451bfd512de1b54da9
jxxxxe <jeun318@gmail.com>
2022-03-03 19:36:01 +0900

    23장 마이바티스 프레임워크

---
+++ pro23/src/com/spring/ex01/MemberDAO.java
@@ -0,0 +1,12 @@
+package com.spring.ex01;
+
+import java.util.List;
+
+public class MemberDAO {
+
+	public List selectAllMemberList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
